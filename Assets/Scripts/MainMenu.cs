9c4b3f15942d165b75da7d0958ef55e2afe00321
Alina Palomino <alinamiap@gmail.com>
2024-11-10 14:26:55 -0600

    Main menu and credits panel

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+
+    public void PlayGame()
+    {
+        //Scene loading and management tbd
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void OpenPortfolio(string url)
+    {
+        Application.OpenURL(url);
+    }
+}

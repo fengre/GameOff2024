cf096b599366198ecfcac1c890fa5fc52c8c3665
Alina Palomino <alinamiap@gmail.com>
2024-12-01 01:06:09 -0600

    Tree closeup adjustment, scene transition

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -6,6 +6,7 @@ public class Item : ScriptableObject
 {
     public string itemName; // Name of the item
     public Sprite itemIcon; // Icon to represent the item in the UI
+    public string color;
 
     [TextArea(3, 10)]
     public string description;
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -8,4 +8,6 @@ public static class PlayerData
     public static HashSet<string> CollectedItems = new HashSet<string>();
     public static bool IsLensToggled = false;
     public static Vector3 LensPosition = Vector3.zero;
+
+    public static HashSet<string> PlacedSecrets = new HashSet<string>();
 }

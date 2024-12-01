d057b4d0d42a0eae2be46da3aedf418b6e8eae41
Connie Xu <clx@princeton.edu>
2024-12-01 15:23:56 -0600

    Final edits

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -6,6 +6,7 @@ public static class PlayerData
 {
     public static string playerName = "";
     public static HashSet<string> CollectedItems = new HashSet<string>();
+    public static HashSet<string> CollectedSecrets = new HashSet<string>();
     public static bool IsLensToggled = false;
     public static Vector3 LensPosition = Vector3.zero;
 
